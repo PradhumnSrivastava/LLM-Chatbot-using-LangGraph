554e37b754de55ddf3f6b8e5ae7446aeeb2adcc2
PradhumnSrivastava <srivastavapradhumn@gmail.com>
2026-08-14 11:01:29 +0530

    edge added

--- graph/graph_builder.py
+++ graph/graph_builder.py
@@ -1,5 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from graph.nodes import generate_response
+from graph.nodes import update_history
 from graph.state import State
 
 
@@ -7,7 +8,9 @@ from graph.state import State
 graph = StateGraph(State)
 
 graph.add_node("generate_response", generate_response)
+graph.add_node("update_history", update_history)
 graph.add_edge(START, "generate_response")
-graph.add_edge("generate_response", END)
+graph.add_edge("generate_response", "update_history")
+graph.add_edge("update_history", END)
 
 workflow = graph.compile()
